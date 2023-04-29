860c90e2249ac0651cd5764c36f79783fe3a3be7
shidurbaba <shidurbaba@gmail.com>
2023-04-29 12:22:18 -0400

    commit 2023 BDD code

--- src/test/java/GreenKartStepDefinitions/stepDefinition.java
+++ src/test/java/GreenKartStepDefinitions/stepDefinition.java
@@ -1,23 +1,31 @@
 package GreenKartStepDefinitions;
 
 import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class stepDefinition {
 
+    public WebDriver driver;
+
     @Given("User is on GreenCart Landing page")
     public void user_is_on_green_cart_landing_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
     }
     @When("User searched with Shortname {string} and extracted actual name of product")
-    public void user_searched_with_shortname_and_extracted_actual_name_of_product(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_searched_with_shortname_and_extracted_actual_name_of_product(String ShortName) {
+
+    driver.findElement(By.xpath("//input[@type='search']")).sendKeys(ShortName);
+    String productName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+    System.out.println(productName + " is extracted from Home page");
+
     }
+
     @Then("User searched for same shortname in offers page to check if product exist")
     public void user_searched_for_same_shortname_in_offers_page_to_check_if_product_exist() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
