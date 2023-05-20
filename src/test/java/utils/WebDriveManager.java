43944d20fc96ddfd778e7ae7b99e9600f1ef5792
shidurbaba <shidurbaba@gmail.com>
2023-05-19 21:51:52 -0400

    Modified BDD frameword 2023

--- src/test/java/utils/WebDriveManager.java
+++ src/test/java/utils/WebDriveManager.java
@@ -3,6 +3,8 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -12,6 +14,8 @@ public class WebDriveManager {
     public WebDriver driver;
     public ChromeOptions chromeOptions;
 
+    public EdgeOptions edgeOptions;
+
     public Properties properties;
 
     public PageObjectManager pageObjectManager;
@@ -19,18 +23,31 @@ public class WebDriveManager {
       this.pageObjectManager = new PageObjectManager(driver);
       this.properties = pageObjectManager.getProperties();
 
-
-      if( properties.getProperty("browser").equalsIgnoreCase("chrome")) {
-          if (driver == null) {
-              this.chromeOptions = new ChromeOptions();
-              chromeOptions.addArguments("--incognito");
-              chromeOptions.addArguments("--start-maximized");
-              chromeOptions.addArguments("--ignore-certificate-errors");
-              chromeOptions.addArguments("--disable-gpu");
-              //chromeOptions.addArguments("--headless");
-              driver = new ChromeDriver(chromeOptions);
-          }
-      }
+    switch (properties.getProperty("browser"))
+    {
+        case "chrome":
+            if (driver == null) {
+                this.chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--incognito");
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.addArguments("--ignore-certificate-errors");
+                chromeOptions.addArguments("--disable-gpu");
+                //chromeOptions.addArguments("--headless");
+                driver = new ChromeDriver(chromeOptions);
+            }
+            break;
+        case "edge":
+            if (driver == null) {
+                this.edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--incognito");
+                edgeOptions.addArguments("--start-maximized");
+                edgeOptions.addArguments("--ignore-certificate-errors");
+                edgeOptions.addArguments("--disable-gpu");
+                //edgeOptions.addArguments("--headless");
+                driver = new EdgeDriver(edgeOptions);
+            }
+            break;
+    }
          return driver;
     }
 
