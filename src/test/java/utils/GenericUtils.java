1b6f7608813d48c850c406b7260b8ccb5e0da51e
shidurbaba <shidurbaba@gmail.com>
2023-05-19 20:53:45 -0400

    commit 2023 BDD code

--- src/test/java/utils/GenericUtils.java
+++ src/test/java/utils/GenericUtils.java
@@ -1,13 +1,19 @@
 package utils;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.Set;
 
 public class GenericUtils {
 
     public WebDriver driver;
+    public WebDriverWait webDriverWait;
 
     public GenericUtils(WebDriver driver)
     {
@@ -23,7 +29,14 @@ public class GenericUtils {
         driver.switchTo().window(childWindow);
     }
 
-
+    public WebElement waitForElementToBeClickable(WebDriver driver, By locator, int timeoutInSeconds) {
+        this.webDriverWait = new WebDriverWait(driver, Duration.ofNanos(timeoutInSeconds));
+        return webDriverWait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+    public WebElement waitForElementToBeVisible(WebDriver driver, By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofNanos(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
 
 
--- src/test/java/utils/WebDriveManager.java
+++ src/test/java/utils/WebDriveManager.java
@@ -2,16 +2,24 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriveManager {
 
     public WebDriver driver;
+    public ChromeOptions chromeOptions;
 
     public  WebDriver WebDriveManager()
     {
          if(driver == null)
          {
-             driver = new ChromeDriver();
+             this.chromeOptions = new ChromeOptions();
+             chromeOptions.addArguments("--incognito");
+             chromeOptions.addArguments("--start-maximized");
+             chromeOptions.addArguments("--ignore-certificate-errors");
+             chromeOptions.addArguments("--disable-gpu");
+             //chromeOptions.addArguments("--headless");
+             driver = new ChromeDriver(chromeOptions);
          }
          return driver;
     }
