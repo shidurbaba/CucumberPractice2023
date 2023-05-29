48720b6083c3e5a1dfa163b634db9e9cc902b66e
shidurbaba <shidurbaba@gmail.com>
2023-05-29 13:18:19 -0400

    commit file

--- src/test/java/RunnerPackage/TestRunner.java
+++ src/test/java/RunnerPackage/TestRunner.java
@@ -1,13 +1,15 @@
 package RunnerPackage;
 
 
+
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(
-        features = "C:\\Users\\moses\\IdeaProjects\\CucumberPractice2023\\src\\test\\java\\feature",
+        features = "src/test/java/feature",
         glue = {"stepDefinitions"},
         plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json"},
         tags = "@Vegetables",
