f9a9d23e3e7a2987755a3334b2ce67afa967abab
shidurbaba <shidurbaba@gmail.com>
2024-01-23 09:54:39 -0500

    New Commit

--- src/test/java/stepDefinitions/CucumberHooks.java
+++ src/test/java/stepDefinitions/CucumberHooks.java
@@ -1,27 +1,33 @@
-package stepDefinitions;
-
-import io.cucumber.java.After;
-import org.openqa.selenium.WebDriver;
-import utils.TestContextSetUp;
-
-
-import java.io.IOException;
-
-public class CucumberHooks {
-
-    private WebDriver driver;
-
-    public  CucumberHooks ( TestContextSetUp contextStepUp) throws IOException {
-        this.driver = contextStepUp.webDriveManager.driver;
-
-    }
-
-
-
-    @After
-    public void AfterScenario() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-}
+//package stepDefinitions;
+//
+//import io.cucumber.java.After;
+//import io.cucumber.java.AfterStep;
+//import org.openqa.selenium.WebDriver;
+//import utils.TestContextSetUp;
+//
+//
+//import java.io.IOException;
+//
+//public class CucumberHooks {
+//
+//    private WebDriver driver;
+//
+//    public  CucumberHooks ( TestContextSetUp contextStepUp) throws IOException {
+//        this.driver = contextStepUp.webDriveManager.driver;
+//
+//    }
+//
+//
+//
+//    @After
+//    public void AfterScenario() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//
+//    @AfterStep
+//    public void AddScreenShot(){
+//        System.out.println("screen shot test");
+//    }
+//}
